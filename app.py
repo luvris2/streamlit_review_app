385317b4e20f7bd7cf50ba81f20007da681f87f5
luvris2 <luvris2@naver.com>
2022-05-24 12:39:04 +0900

    add

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+from fbprophet import Prophet
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
+
+# 파이썬 가상환경 확인
+# 이 앱은 구글 코랩에서 만든 아보카도 가격 예측 환경과 동일
+# 파이썬 가상환경을 streamlit 3.7로 변경
+# vs code의 가상환경 맞춰 변경
